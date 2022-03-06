5846f6c6ad6b867e00444889e3493ff2f5e798de
Okens <Ng.Eric314@gmail.com>
2022-03-05 23:51:28 -0800

    Adding initial info call and function structures.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,92 @@
 var searchButton = document.getElementById("searchButton");
 var searchList = document.getElementById("searchList");
 
-var needDefaultStorage = true;
+var defaultInfoStorage = false;
 var searchStorage = getStorage("searchList");
 var infoStorage = getStorage("infoList");
 
+// defaultInfoStorage is true for initial load.
+if (infoStorage.length > 0) {
+  defaultInfoStorage = true;
+}
 
-searchButton.addEventListener("click", function(e){
-  console.log("search button clicked")
-  // TODO: Run function to make API call.
-  console.log(config.TEMP_KEY)
-})
+displaySearchedCities();
+displayDefaultInfo();
+
+searchButton.addEventListener("click", function (e) {
+  var city = document.getElementById("cityName").value;
+  if (city.length > 0) {
+    searchStorage.unshift(city);
+    // limit max number of search city buttons to 8
+    if (searchStorage.length > 8) {
+      searchStorage = searchStorage.slice(0, 8);
+    }
+    console.log("search button clicked");
+    // TODO: Run function to make API call.
+    saveToStorage();
+    displaySearchedCities();
+  }
+});
 
 // Add event listener to search city buttons.
 document.addEventListener("DOMContentLoaded", function() {
   searchList.addEventListener("click", function(e){
     if(e.target.classList.contains("button-list")){
+      var city = e.target.innerText;
       console.log("city button clicked")
+      console.log("city: ", city)
       // TODO: Run function to make API call.
     }
   })
 });
 
 // TODO: Add search buttons from storage.
+function displaySearchedCities() {
+  searchList.innerHTML = "";
+  for(var i=0; i < searchStorage.length; i++){    
+    var liBtnStr = `<li><button class="color-tertiary button button-list">${searchStorage[i]}</button></li>`;
+    searchList.insertAdjacentHTML('beforeend', liBtnStr)
+  }
+}
+
 
 // Call display data with infoStorage as default.
-
-// TODO: Function to make api call.
-  // Call api
-  // Call display data
+function displayDefaultInfo(){
+  if(defaultInfoStorage === true){
+    displayPresent();
+    displayForecast();
+  }
+}
 
 // TODO: Function to display data. Calls present and forecast.
-  // Call display present
-  // Call display forecast
+function displayInfo(){
+  console.log("function displayInfo")
+  displayPresent();
+  displayForecast();
+}
+
+// TODO: Function to make api call.
+function getWeatherData(){
+  console.log("function getWeatherData")
+  // Call api
+  // Call display data
+}
 
 
 // TODO: Function to display present.
+function displayPresent(){
+  console.log("function displayPresent")
+}
 // TODO: Function to display forecast.
+function displayForecast(){
+  console.log("function displayForecast")
+}
 
 
 function getStorage(storageItem) {
   var storageData = JSON.parse(localStorage.getItem(storageItem));
   if (storageData === null) {
     storageData = [];
-  } else {
-    needDefaultStorage = false;
   }
 
   return storageData;
@@ -54,9 +96,9 @@ function saveToStorage() {
   var currentSearchStorage = searchStorage;
   var currentInfoStorage = infoStorage;
 
-  if (currentStorage.length > 1) {
-    // currentStorage.sort(compareDescending);
-  }
+  // if (currentInfoStorage.length > 1) {
+    // currentSearchStorage.sort(compareDescending);
+  // }
   localStorage.setItem("searchList", JSON.stringify(currentSearchStorage));
   localStorage.setItem("infoList", JSON.stringify(currentInfoStorage));
 }
