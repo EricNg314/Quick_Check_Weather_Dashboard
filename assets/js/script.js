ab0e8ecb9864635b976305843ea7553e3eda656b
Okens <Ng.Eric314@gmail.com>
2022-03-05 19:12:06 -0800

    Adding storage function and event listeners to buttons.

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+var searchButton = document.getElementById("searchButton");
+var searchList = document.getElementById("searchList");
+
+var needDefaultStorage = true;
+var searchStorage = getStorage("searchList");
+var infoStorage = getStorage("infoList");
+
+
+searchButton.addEventListener("click", function(e){
+  console.log("search button clicked")
+  // TODO: Run function to make API call.
+})
+
+// Add event listener to search city buttons.
+document.addEventListener("DOMContentLoaded", function() {
+  searchList.addEventListener("click", function(e){
+    if(e.target.classList.contains("button-list")){
+      console.log("city button clicked")
+      // TODO: Run function to make API call.
+    }
+  })
+});
+
+// TODO: Add search buttons from storage.
+
+// Call display data with infoStorage as default.
+
+// TODO: Function to make api call.
+  // Call api
+  // Call display data
+
+// TODO: Function to display data. Calls present and forecast.
+  // Call display present
+  // Call display forecast
+
+
+// TODO: Function to display present.
+// TODO: Function to display forecast.
+
+
+function getStorage(storageItem) {
+  var storageData = JSON.parse(localStorage.getItem(storageItem));
+  if (storageData === null) {
+    storageData = [];
+  } else {
+    needDefaultStorage = false;
+  }
+
+  return storageData;
+}
+
+function saveToStorage() {
+  var currentSearchStorage = searchStorage;
+  var currentInfoStorage = infoStorage;
+
+  if (currentStorage.length > 1) {
+    // currentStorage.sort(compareDescending);
+  }
+  localStorage.setItem("searchList", JSON.stringify(currentSearchStorage));
+  localStorage.setItem("infoList", JSON.stringify(currentInfoStorage));
+}
+
+function compareDescending(a, b) {
+  // sort current object and next object by time key
+  if (a.date > b.date) {
+    return -1;
+  }
+  if (a.date < b.date) {
+    return 1;
+  }
+  return 0;
+}
